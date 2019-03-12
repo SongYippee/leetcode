536c7978845db6360186e616818742869bdd785c
Song Yippee <yinpsong@cisco.com>
2019-03-12 14:56:09 +0800

    add 排列字符串，待实现

---
+++ String/排列字符串.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/4 17:06
+# @Author  : Yippee Song
+# @Software: PyCharm
+
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        # TODO implement
+
+
+if __name__ == '__main__':
+    s1 = "ab"
+    s2 = "eidbaooo"
+    print Solution().checkInclusion(s1, s2)
