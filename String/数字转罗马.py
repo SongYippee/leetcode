fe41b00507e9e8a785db70d3d428c189baac4880
Yippee Song <34080396+SongYippee@users.noreply.github.com>
2019-03-24 13:14:45 +0800

    Create 数字转罗马.py
    add 数字转罗马文

---
+++ String/数字转罗马.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        symbols = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX", 10:"X", 40:"XL", 50:"L", 90:"XC", 100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"}
+        keys = sorted(symbols, reverse=True)
+        result = {}
+        result_key = []
+        for item in keys:
+            shang = num / item
+            yushu = num % item
+            num = yushu
+            if shang > 0:
+                result[item] = shang
+                result_key.append(item)
+        result_str = ""
+        for item in result_key:
+            result_str += result[item] * symbols[item]
+        return result_str
