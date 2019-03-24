396973652b4fe541b05f9a09f407b227ffadcf6a
Yippee Song <34080396+SongYippee@users.noreply.github.com>
2019-03-24 13:16:14 +0800

    add 实例

--- String/数字转罗马.py
+++ String/数字转罗马.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        
+        """
+        Example:
+            Input: 1994
+            Output: "MCMXCIV"
+            Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+        """
+        
         symbols = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX", 10:"X", 40:"XL", 50:"L", 90:"XC", 100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"}
         keys = sorted(symbols, reverse=True)
         result = {}
