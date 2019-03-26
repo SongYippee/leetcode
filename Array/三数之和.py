738b99b44bdfac34843a0e5a3b648623014a2b2c
Song Yippee <yinpsong@cisco.com>
2019-03-26 10:42:22 +0800

    add 翻转图片

--- Array/三数之和.py
+++ Array/三数之和.py
@@ -15,7 +15,7 @@
   [-1, -1, 2]
 ]
 '''
-
+import collections
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -39,19 +39,19 @@ class Solution(object):
         return  ans
       
       
-  def threeSum_priority(self, nums):
-	    if len(nums) < 3: return []
-	    counter = collections.Counter(nums)
-	    tri = [[0,0,0]] if counter[0] > 2 else []
-	    neg, pos = [x for x in counter if x < 0], [x for x in counter if x >= 0]
-	    for n in neg:
-		    for p in pos:
-		    	x = -n-p
-			    if x in counter:
-			    	if x in {n, p} and counter[x] > 1: tri.append([n,x,p])
-			    	if x < n: tri.append([x,n,p])
-			    	if x > p: tri.append([n,p,x])
-	    return tri
+    def threeSum_priority(self, nums):
+        if len(nums) < 3: return []
+        counter = collections.Counter(nums)
+        tri = [[0,0,0]] if counter[0] > 2 else []
+        neg, pos = [x for x in counter if x < 0], [x for x in counter if x >= 0]
+        for n in neg:
+            for p in pos:
+                x = -n-p
+                if x in counter:
+                    if x in {n, p} and counter[x] > 1: tri.append([n,x,p])
+                    if x < n: tri.append([x,n,p])
+                    if x > p: tri.append([n,p,x])
+        return tri
 
 if __name__ == '__main__':
     nums = [-1, 0, 1, 2, -1, -4]
