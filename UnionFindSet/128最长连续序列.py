ff9ec7cd54a0517e7acde8c9457a97a511c053a3
Yinping Song (yinpsong) <yinpsong@cisco.com>
2019-12-24 11:00:47 +0800

    add 11盛最多水的容器

--- UnionFindSet/128最长连续序列.py
+++ UnionFindSet/128最长连续序列.py
@@ -27,20 +27,20 @@ class Solution(object):
         max_length = 0
         for num in nums:
             if num not in hash_dict:
-                left = hash_dict.get(num - 1, 0)
-                right = hash_dict.get(num + 1, 0)
+                left = hash_dict.get(num - 1, 0)  # 左边界的连续长度
+                right = hash_dict.get(num + 1, 0)  # 右边界的连续长度
 
                 cur_length = 1 + left + right
                 if cur_length > max_length:
                     max_length = cur_length
 
                 hash_dict[num] = cur_length
-                hash_dict[num - left] = cur_length
-                hash_dict[num + right] = cur_length
+                hash_dict[num - left] = cur_length  # 当前连续范围的左边界，设置连续长度
+                hash_dict[num + right] = cur_length  # 当前连续范围的右边界，设置连续长度
 
         return max_length
 
 
 if __name__ == '__main__':
-    nums = [100, 4, 200, 1, 3, 2, 2, 5]
+    nums = [100, 4, 200, 1, 3, 2, 2, 5, 101, 98, 99, 97]
     print Solution().longestConsecutive(nums)
